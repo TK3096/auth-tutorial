9ed3b55e7e3d2692e6d1bc1a0fc87ccf695e20a6
tongthanaphon <tongthanaphon.tk@gmail.com>
2024-02-27 23:33:19 +0700

    add; next-auth

--- src/actions/register.ts
+++ src/actions/register.ts
@@ -1,8 +1,12 @@
 'use server'
 
 import * as z from 'zod'
+import bcrypt from 'bcryptjs'
 
 import { RegisterSchema } from '@/schemas'
+
+import { db } from '@/lib/db'
+import { getUserByEmail } from '@/data/user'
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validatedFields = RegisterSchema.safeParse(values)
@@ -11,5 +15,25 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
     return { error: 'Invalid fields' }
   }
 
-  return { success: 'Email sent' }
+  const { email, password, name } = validatedFields.data
+
+  const existingUser = await getUserByEmail(email)
+
+  if (existingUser) {
+    return { error: 'Email already exists' }
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10)
+
+  await db.user.create({
+    data: {
+      email,
+      name,
+      password: hashedPassword,
+    },
+  })
+
+  // TODO: send email token
+
+  return { success: 'User created' }
 }
